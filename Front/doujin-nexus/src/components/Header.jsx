70830fcf312341a958742846b419b74148fc4210
Grégory Febvin <greg.feb20@gmail.com>
2025-07-29 11:10:36 +0200

    Change headers route for new api calls

--- Front/doujin-nexus/src/components/Header.jsx
+++ Front/doujin-nexus/src/components/Header.jsx
@@ -1,15 +1,19 @@
 // Header.jsx
+import { useNavigate } from 'react-router-dom';
+
 import logo from '../assets/logo.png';
 import glass from '../assets/glass.svg';
 
 export default function Header() {
+  const navigate = useNavigate();
+
   return (
     <div className="header">
-      <img className="img-logo" src={logo} alt="Logo Doujin Nexus" />
+      <img className="img-logo" src={logo} alt="Logo Doujin Nexus" style={{ cursor: 'pointer' }} onClick={() => navigate(`/`)}/>
       <form onSubmit={(e) => e.preventDefault()}>
         <input type="text" className="search-bar" placeholder="Recherche…" />
         <button className="btn btn-search" type="submit">
-          <img className="img-search-button" src={glass} alt="Rechercher" />
+          <i className="fa fa-search fa-lg" />
         </button>
       </form>
     </div>
--- Front/doujin-nexus/src/components/Tabs.jsx
+++ Front/doujin-nexus/src/components/Tabs.jsx
@@ -1,15 +1,19 @@
 // Tabs.jsx
+import { useNavigate } from 'react-router-dom'
+
 export default function Header() {
+  const navigate = useNavigate();
+
   return (
     <div className="tabs">
         <div className="link">
-            <div className="tab"><a href="#">Aléatoire</a></div>
-            <div className="tab"><a href="#">Séries</a></div>
-            <div className="tab"><a href="#">Tags</a></div>
-            <div className="tab"><a href="#">Personnages</a></div>
-            <div className="tab"><a href="#">Artistes</a></div>
-            <div className="tab"><a href="#">Groupes</a></div>
-            <div className="tab"><a href="#">Aide</a></div>
+            <div className="tab"><a style={{ cursor: 'pointer' }} onClick={() => navigate(`/random`)}>Aléatoire</a></div>
+            <div className="tab"><a style={{ cursor: 'pointer' }} onClick={() => navigate(`/parodies`)}>Séries</a></div>
+            <div className="tab"><a style={{ cursor: 'pointer' }} onClick={() => navigate(`/tags`)}>Tags</a></div>
+            <div className="tab"><a style={{ cursor: 'pointer' }} onClick={() => navigate(`/characters`)}>Personnages</a></div>
+            <div className="tab"><a style={{ cursor: 'pointer' }} onClick={() => navigate(`/artists`)}>Artistes</a></div>
+            <div className="tab"><a style={{ cursor: 'pointer' }} onClick={() => navigate(`/groups`)}>Groupes</a></div>
+            <div className="tab"><a style={{ cursor: 'pointer' }} onClick={() => navigate(`/info`)}>Aide</a></div>
         </div>
     </div>
   );
